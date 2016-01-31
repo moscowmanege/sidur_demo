3077e641085ccac4135a5047c6501887b8f0bc0d
ponyspy <desade4me@gmail.com>
2016-01-31 16:53:24 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+	var parent = $('.infinity');
+	var max = parent[0].scrollHeight - parent[0].offsetHeight - 20;
 
+	parent.on('scroll', function(event) {
+	    var s = $(this).scrollTop(),
+	        f = $('>:first', parent),l = $('>:last', parent);
+	    if(s > max) {f.appendTo(parent); parent.scrollTop(s - f.height())}
+	    if(s < 5) {l.prependTo(parent);parent.scrollTop(s + l.height()) }
+	}).scrollTop(5);
 });
